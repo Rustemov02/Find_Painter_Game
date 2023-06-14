74ddb718c8e4e9afab042db7618107760beb2c86
Rustemov02 <bunyamin2002@mail.ru>
2023-06-15 00:05:24 +0400

    completed

--- finder/src/Answer.jsx
+++ finder/src/Answer.jsx
@@ -2,8 +2,7 @@ import React from 'react'
 import qBank from './QuestionsBank'
 
 export default function Answer() {
-
-
+ 
     return (
         <div style={{display : 'flex',flexDirection : 'row' , gap : 10}}>
             {qBank.map(item => <p key={item.id} style={{fontSize : 20,fontWeight : 600, fontFamily : 'Quicksand'}}>{item.id}.{item.answer}</p>
